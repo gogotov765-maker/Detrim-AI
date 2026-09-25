494950f708390cdead091422ff3fcab57eef74b2
gogotov765-maker <gogotov765@gmail.com>
2026-09-26 01:32:39 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    id("com.android.application") version
+"8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version
+"1.9.24" apply false
+}
+
 android {
     namespace = "com.detrim.ai"
     compileSdk = 34
